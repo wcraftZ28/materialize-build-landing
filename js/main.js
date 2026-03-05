8cce9819dce8e9cc4b97492e9720c32c7d2a006f
wcraftZ28 <borozdinaes89@yandex.ru>
2026-03-05 14:29:00 +0500

    menu correct

--- js/main.js
+++ js/main.js
@@ -1,4 +1,7 @@
 document.addEventListener('DOMContentLoaded', function() {
-    var elems = document.querySelectorAll('.sidenav');
-    var instances = M.Sidenav.init(elems);
+    var sidenav = document.querySelectorAll('.sidenav');
+    var instances_sidenav = M.Sidenav.init(sidenav);
+
+    var modal = document.querySelectorAll('.modal');
+    var instances_modal = M.Modal.init(modal);
   });
